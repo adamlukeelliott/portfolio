1c704a3b62bcd506eef10f2db46a977bd4bf2018
adamlukeelliott <adamelliott443@gmail.com>
2025-04-11 21:59:51 +0100

    Mobile friendly password page, styling updates

--- src/app/password-checker/password-check.tsx
+++ src/app/password-checker/password-check.tsx
@@ -15,27 +15,37 @@ const PasswordForm = () => {
   const [score, setScore] = useState<number>(0);
   const [widthClass, setWidthClass] = useState<string>("w-0");
   const [strengthText, setStrengthText] = useState<string>("");
+  const [strengthColour, setStrengthColour] = useState<string>("bg-red-500");
+
+  const widthMap: Record<number, string> = {
+    0: "w-0",
+    1: "w-1/4",
+    2: "w-2/4",
+    3: "w-3/4",
+    4: "w-full",
+  };
+
+  const strengthTextMap: Record<number, string> = {
+    0: "",
+    1: "Weak",
+    2: "Moderate",
+    3: "Strong",
+    4: "Very Strong",
+  };
+
+  const strengthColourMap: Record<number, string> = {
+    1: "bg-red-500",
+    2: "bg-orange-500",
+    3: "bg-yellow-500",
+    4: "bg-green-500",
+  };
 
   useEffect(() => {
     setScore(zxcvbn(formData.password).score);
-    if (score > 0) {
-      setWidthClass(`w-${score}/4`);
-      setStrengthText(getStrengthTextString(score));
-    } else {
-      setWidthClass("w-0");
-      setStrengthText("");
-    }
+    setWidthClass(widthMap[score]);
+    setStrengthText(strengthTextMap[score]);
+    setStrengthColour(strengthColourMap[score]);
   }, [formData]);
-
-  function getStrengthTextString(score: number): string {
-    switch (score) {
-      case 1: return "Weak";
-      case 2: return "Moderate";
-      case 3: return "Strong";
-      case 4: return "Very Strong";
-      default: return "";
-    }
-  }
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -53,7 +63,7 @@ const PasswordForm = () => {
         autoComplete="off"
         value={formData.password}
         onChange={handleChange}
-        className="text-center w-xl text-6xl bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="text-center w-full text-lg bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="Enter password..."
       />
 
@@ -62,7 +72,7 @@ const PasswordForm = () => {
       </h3>
 
       <div className="w-full h-8 bg-gray-200 rounded-full dark:bg-gray-700">
-        <div className={`h-8 bg-blue-600 rounded-full dark:bg-blue-500 transition-all ${widthClass} flex items-center justify-center transition`}>
+        <div className={`transition-all h-8 ${strengthColour} rounded-full ${widthClass} flex items-center justify-center`}>
           {strengthText}
         </div>
       </div>
