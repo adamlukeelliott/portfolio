36e767f9174703dbbe64f9c670f6abcaba0e4a62
adamlukeelliott <adam.elliott443@gmail.com>
2025-05-07 00:02:41 +0100

    Hide contact form on submit

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,14 +9,15 @@ export default function Home() {
 
   const [form, setForm] = useState({ message: '', email: '' });
   const [, setResponse] = useState('');
+  const [contactVisible, setContactVisible] = useState(true);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
     setForm(prev => ({ ...prev, [name]: value }));
   };
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
 
     const res = await fetch('/api/send', {
       method: 'POST',
@@ -28,6 +29,8 @@ export default function Home() {
 
     const data = await res.json();
     setResponse(`Server says: ${JSON.stringify(data.received)}`);
+
+    setContactVisible(false);
   };
 
   return (
@@ -35,18 +38,21 @@ export default function Home() {
       <Navbar />
 
       <Background>
-        <div className="text-center">
+        {!contactVisible && ( 
+          <h1 className="text-5xl text-center font-extrabold dark:text-white">
+            Thank you, your message has been sent!
+          </h1>)}
+        {contactVisible && ( <><div id="contact-form" className="text-center">
           <h1 className="text-5xl text-center font-extrabold dark:text-white">
             Contact
           </h1>
-        </div>
-        <form onSubmit={handleSubmit} className="flex flex-col items-center mt-5">
-          <label htmlFor="email" className="text-xl">E-Mail:</label>
-          <input required pattern="[^ @]*@[^ @]*" type="text" name="email" onChange={handleChange} className="dark:bg-gray-700 p-1 border-gray-400 border dark:border-none rounded-md w-[90%] lg:w-[50%]"></input><br/>
-          <label htmlFor="message" className="text-xl">Message:</label>
-          <textarea required name="message" onChange={handleChange} className="dark:bg-gray-700 p-1 border border-gray-400 dark:border-none rounded-md w-[90%] lg:w-[50%] h-50"></textarea><br/>
-          <input type="submit" className="transition bg-transparent dark:hover:bg-gray-600 hover:bg-gray-200 content-center border border-gray-400 rounded-md p-3 w-50"/>
-        </form>
+        </div><form onSubmit={handleSubmit} className="flex flex-col items-center mt-5">
+            <label htmlFor="email" className="text-xl">E-Mail:</label>
+            <input required pattern="[^ @]*@[^ @]*" type="text" name="email" onChange={handleChange} className="dark:bg-gray-700 p-1 border-gray-400 border dark:border-none rounded-md w-[90%] lg:w-[50%]"></input><br />
+            <label htmlFor="message" className="text-xl">Message:</label>
+            <textarea required name="message" onChange={handleChange} className="dark:bg-gray-700 p-1 border border-gray-400 dark:border-none rounded-md w-[90%] lg:w-[50%] h-50"></textarea><br />
+            <input type="submit" className="transition bg-transparent dark:hover:bg-gray-600 hover:bg-gray-200 content-center border border-gray-400 rounded-md p-3 w-50" />
+          </form></> )}
       </Background>
       <Footer />
     </div>
